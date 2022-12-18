f256202b838ab08a0b204a0fe577bacd03b012ec
Ryan Djemtchemeu <ryanmambou@gmail.com>
2022-12-18 10:18:37 +0100

    loop icon fix

--- Components/NavBar.jsx
+++ Components/NavBar.jsx
@@ -13,7 +13,9 @@ function NavBar({navLinks}) {
                 <li key={index} className="px-2 cursor-pointer">{link}</li>
                ))}
             </ul>
-            <Image src={loopImg} alt="loop-icon" className="ml-2 bg-green-100 rounded-3xl cursor-pointer p-4"/>
+            <div className='cursor-pointer ml-2 bg-green-100 rounded-3xl p-2 flex justify-center items-center'>
+                <Image src={loopImg} alt="loop-icon"/>
+                </div>
             <button className='bg-green-700 text-white rounded-md py-2 px-5 border 2 ml-5 cursor-pointer'>Login</button>
             <button className='border-white border-2 text-green-900 hover:border-green-700 hover:bg-gray-100 rounded-md py-2 px-5 ml-2 cursor-pointer'>Sign up</button>
         </div>
