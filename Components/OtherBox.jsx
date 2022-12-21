64a5faa1a5025ecfea5570d7a31f77073855f362
Ryan Djemtchemeu <ryanmambou@gmail.com>
2022-12-21 08:35:43 +0100

    responsive fix

--- Components/OtherBox.jsx
+++ Components/OtherBox.jsx
@@ -3,10 +3,10 @@ import Image from 'next/image'
 
 function OtherBox({coverImage, heading, paragraph}) {
   return (
-    <div className='group h-full'>
-        <div className='rounded-xl overflow-hidden h-[20rem]'><Image src={coverImage} alt="coverImage" className='object-fit rounded-xl'/></div>
-        <h4 className='text-xl font-medium group-hover:text-green-700'>{heading}</h4>
-        <p className='font-thin text-gray-700 transition-all duration-150 ease-in'>{paragraph}</p>
+    <div className='group h-full cursor-pointer'>
+        <div className='rounded-xl overflow-hidden'><Image src={coverImage} alt="coverImage" className='object-cover h-[18.75rem] rounded-xl'/></div>
+        <h4 className='text-xl font-medium group-hover:text-green-700 pt-1 pb-3'>{heading}</h4>
+        <p className='font-thin text-gray-700'>{paragraph}</p>
     </div>
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -19,25 +19,25 @@ export default function Home() {
     <NavBar navLinks={navLinks} />
     <Banner />
     <Partners />
-    
-    <div className='md:px-12 px-8 flex flex-col gap-4 items-start'>
+
+    <div className='md:px-12 px-8 flex flex-col gap-4 items-start justify-center'>
     <Headers heading="Discover popular articles" paragraph="These articles review the latest news about the Framework, but also feedback on our business. Discover the most popular articles shared by members of the community."/>
-    <div className='grid grid-col-1 md:grid-cols-3 md:grid-rows-2 gap-3 w-full h-[26rem]'>
+    <div className='grid grid-col-1 md:grid-cols-3 md:grid-rows-2 gap-3 w-full'>
       <div className="md:row-span-2"><MagicBox coverImage={blogImg} heading="Introducing flutter in focus"/></div>
-      <MagicBox coverImage={blogImg2} heading="Introducing flutter in focus"/>
+      <div className="md:row-span-1"><MagicBox coverImage={blogImg2} heading="Introducing flutter in focus"/></div>
       <div className="md:row-span-2"><MagicBox coverImage={blogImg3} heading="Introducing flutter in focus"/></div>
-      <MagicBox coverImage={blogImg4} heading="Introducing flutter in focus"/>
+      <div className="md:row-span-1"><MagicBox coverImage={blogImg4} heading="Introducing flutter in focus"/></div>
     </div>
     <button className='rounded text-white bg-green-800 py-2 px-5 cursor-pointer'>See all articles</button>
     </div>
 
     <div className='md:px-12 px-8 flex flex-col gap-4 items-start'>
       <Headers heading="Community events" paragraph="Find out about all the virtual and physical events organized by the different communities in each city. Attend events hosted by the global Flutter community and continue to learn more about Flutter."/>
-      <div className='grid md:grid-cols-9 grid-rows-2 gap-4'>
-        <div className="col-span-5"><OtherBox coverImage={blogImg2} heading="The heading" paragraph="Baby calm down!"/></div>
-        <div className="col-span-4"><OtherBox coverImage={blogImg2} heading="The heading" paragraph="Baby calm down!"/></div>
-        <div className="col-span-4"><OtherBox coverImage={blogImg2} heading="The heading" paragraph="Baby calm down!"/></div>
-        <div className="col-span-5"><OtherBox coverImage={blogImg2} heading="The heading" paragraph="Baby calm down!"/></div>
+      <div className='grid md:grid-cols-9 grid-rows-2 gap-8 md:gap-4'>
+        <div className="md:col-span-5"><OtherBox coverImage={blogImg2} heading="The heading" paragraph="Baby calm down!"/></div>
+        <div className="md:col-span-4"><OtherBox coverImage={blogImg2} heading="The heading" paragraph="Baby calm down!"/></div>
+        <div className="md:col-span-4"><OtherBox coverImage={blogImg2} heading="The heading" paragraph="Baby calm down!"/></div>
+        <div className="md:col-span-5"><OtherBox coverImage={blogImg2} heading="The heading" paragraph="Baby calm down!"/></div>
         </div>
     </div>
     </div>
