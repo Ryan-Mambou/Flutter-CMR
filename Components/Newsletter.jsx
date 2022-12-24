3b26b1e3da4345fec268d42341adc28abaff9366
Ryan Djemtchemeu <ryanmambou@gmail.com>
2022-12-24 09:01:32 +0100

    fix newsletter

--- Components/Newsletter.jsx
+++ Components/Newsletter.jsx
@@ -8,7 +8,7 @@ function Newsletter() {
         <Image src={backgrounSvg} alt="background" className='absolute top-0'/>
         <h2 className='text-white font-medium text-[2.6rem]'>Join our newsletter</h2>
         <span className='text-white text-lg'>Join our newsletter to receive tutorials, articles and podcasts on web design and programming.</span>
-        <div className='flex sm:flex-row flex-col gap-2 sm:w-full'>
+        <div className='flex sm:flex-row flex-col gap-2'>
             <input type="text" className='rounded-lg px-5 py-2 z-10 focus:ring-gray-500 focus:outline-gray-400 text-gray-500 font-semibold' placeholder='Enter your email address' />
             <button type='submit' className='rounded-lg py-2 px-4 z-10 cursor-pointer bg-green-500 text-green-800 hover:bg-green-600 transition-all ease-linear'>Sign up</button>
         </div>
